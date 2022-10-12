8bd23e63cc0c81b06c159f576de40aaf17053b3f
Bishal Keshri <bishal.keshri@nagarro.com>
2022-10-12 22:57:12 +0530

    Learnt Different methods for setter injection in config.xml file those are one is in full normal way, another is is value as property way and last is  p:schema way is the most optimized

--- src/main/java/com/bishal/spring/Controller.java
+++ src/main/java/com/bishal/spring/Controller.java
@@ -12,8 +12,13 @@ public class Controller
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");   // now this context is our spring container(IoC container), jisko hum directly bol skte hai ki ap mujhe config.xml sae us student ka object lakr dedo, so we directly write below ki context.getBean("the object name, i.e. student1 as we made it in name inside config.xml file)
-        Student student = (Student) context.getBean("student1");
-        System.out.println(student);	
+        @SuppressWarnings("resource")
+		ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");   // now this context is our spring container(IoC container), jisko hum directly bol skte hai ki ap mujhe config.xml sae us student ka object lakr dedo, so we directly write below ki context.getBean("the object name, i.e. student1 as we made it in name inside config.xml file)
+        Student student1 = (Student) context.getBean("student1");
+        Student student2 = (Student) context.getBean("student2");
+        Student student3 = (Student) context.getBean("student3");
+        System.out.println(student1);	
+        System.out.println(student2);	
+        System.out.println(student3);
     }
 }
